0730ab89891db87eff9522b6e214c6ec020f6448
DandiKurnia <dandikurnia608@gmail.com>
2022-09-13 13:27:45 +0700

    lesson-7

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const hadleClick = (e) =>{
+        console.log('hello, ninjas', e);
+    }
+
+    const handleClickAgain = (name, e)=>{
+        console.log('hello ' + name, e.target);
+    }
+
     return (
         <div className="home">
             <h2>Home page</h2>
+            <button onClick={hadleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('dandi', e)}>Click me again</button>
         </div>
     );
 }
